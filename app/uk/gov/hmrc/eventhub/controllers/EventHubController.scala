9c3d386d7de905653332916e3f47ecf02f1d48c7
Jim Collins <jim.r.collins@gmail.com>
2021-06-04 12:15:13 +0100

    first commit

---
+++ app/uk/gov/hmrc/eventhub/controllers/EventHubController.scala
@@ -0,0 +1,37 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.eventhub.controllers
+
+import play.api.mvc.{AnyContent, BaseController, ControllerComponents, Request}
+
+import javax.inject.{Inject, Singleton}
+import uk.gov.hmrc.workitem._
+
+@Singleton
+class EventHubController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+
+  /**
+   * Create an Action to render an HTML page.
+   *
+   * The configuration in the `routes` file means that this method
+   * will be called when the application receives a `GET` request with
+   * a path of `/`.
+   */
+  def publishEvent(topic: String) = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index())
+  }
+}
---
+++ build.sbt
@@ -0,0 +1,29 @@
+import sbt.Resolver
+
+name := """event-hub"""
+organization := "uk.gov.hmrc"
+
+version := "1.0-SNAPSHOT"
+
+lazy val root = (project in file(".")).enablePlugins(PlayScala)
+
+scalaVersion := "2.12.12"
+
+libraryDependencies ++= AppDependencies.compile ++ AppDependencies.test
+libraryDependencies += guice
+
+resolvers += MavenRepository("HMRC-open-artefacts-maven2", "https://open.artefacts.tax.service.gov.uk/maven2")
+resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefacts.tax.service.gov.uk/ivy2"))(Resolver.ivyStylePatterns)
+
+resolvers ++= Seq(
+  Resolver.jcenterRepo,
+  Resolver.bintrayRepo("hmrc", "releases"),
+  Resolver.bintrayRepo("jetbrains", "markdown"),
+  "bintray-djspiewak-maven" at "https://dl.bintray.com/djspiewak/maven"
+)
+
+// Adds additional packages into Twirl
+//TwirlKeys.templateImports += "uk.gov.hmrc.controllers._"
+
+// Adds additional packages into conf/routes
+// play.sbt.routes.RoutesKeys.routesImport += "uk.gov.hmrc.binders._"
